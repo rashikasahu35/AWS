280ab1b7af15e287e374d25cee4932e58c5bc2a4
rashika.sahu <rashika.sahu@nagarro.com>
2025-01-10 19:35:28 +0530

    first commit - adding files

---
+++ Pythonapp-on-EC2-with-AWS-DevOps/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+sudo docker pull rashikasahu/python-app:latest
+
+# # Run the Docker image as a container
+sudo docker run -d -p 5000:5000 --name python-app rashikasahu/python-app:latest
---
+++ Pythonapp-on-EC2-with-AWS-DevOps/stop_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+sudo docker stop python-app
+sudo docker rm python-app
+
